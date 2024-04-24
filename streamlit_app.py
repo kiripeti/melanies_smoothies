0bdb6e79ebddd347a4fb19939ac79b2a11099816
kiripeti <kiralypeter85@gmail.com>
2024-04-24 18:00:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -33,3 +34,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_inser_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
